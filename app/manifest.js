8e3309cd8e6432342dcff7f68eafe80a6756e2ef
Avnish Kumar <theavnishkumar@users.noreply.github.com>
2025-06-25 20:54:31 +0530

    update manifest

--- app/manifest.js
+++ app/manifest.js
@@ -2,7 +2,7 @@ export default function manifest() {
     return {
         name: 'Avnish Kumar - Web Developer',
         short_name: 'Avnish Kumar',
-        description: "I'm Avnish Kumar, a passionate and dedicated 4th-year Computer Science student at Gyan Ganga Institute of Technology and Sciences, Jabalpur. Avnish Kumar is a Full Stack web developer from Buxar, Bihar.",
+        description: "Avnish Kumar is a Full Stack Web Developer from Buxar, Bihar, and a 2024 Computer Science graduate from Gyan Ganga Institute of Technology and Sciences, Jabalpur. He builds fast, clean, and responsive web apps using React, Node.js, Express, MongoDB, NestJS, and TypeScript.",
         start_url: '/',
         display: 'standalone',
         background_color: '#fff',
